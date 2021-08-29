ee5590a60157bec7efb54f127202c2a0835b9f46
Adrian Cruz <drincruz@gmail.com>
2021-08-29 19:36:36 -0400

    Adding the first REST controller example
    Baked into `DemoApplication` for now.

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -2,9 +2,23 @@ package demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
+
+data class Message(val id: String?, val text: String)
+
+@RestController
+class MessageResource {
+    @GetMapping
+    fun index(): List<Message> = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
